eebbfd8590a26a53f07dc06eed782ccbd3147c9a
Zaiba Iqbal <33644518+zaiba19@users.noreply.github.com>
2019-01-08 21:54:17 -0500

    Add files via upload

---
+++ prettyFlower.py
@@ -0,0 +1,14 @@
+#This outputs a very pretty flower
+
+import turtle
+
+tina=turtle.Turtle()
+
+for i in range(18):
+	tina.forward(100)
+	tina.left(145)
+	tina.forward(10)
+	tina.right(90)
+	tina.forward(10)
+	tina.left(135)
+	tina.forward(10)
